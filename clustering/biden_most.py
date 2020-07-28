6dc6b9be843cd5186716a6a1531f7ef62cbd8bf8
amontalb <alyssa.montalbano@cern.ch>
2020-07-28 12:13:04 -0700

    checking association with DJI

---
+++ clustering/biden_most.py
@@ -0,0 +1,60 @@
+Top terms per cluster (out of5:
+Cluster 0: 
+ trump
+ nation
+ president
+ america
+ klan
+ said
+ hate
+ white
+ charlottesville
+ supremacists
+Cluster 1: 
+ need
+ workers
+ people
+ president
+ going
+ get
+ jobs
+ crisis
+ health
+ trump
+Cluster 2: 
+ going
+ america
+ george
+ know
+ one
+ floyd
+ us
+ people
+ president
+ type
+Cluster 3: 
+ campaign
+ south
+ democratic
+ want
+ party
+ carolina
+ america
+ democrats
+ tonight
+ thank
+Cluster 4: 
+ fourth
+ idea
+ type
+ founded
+ chance
+ independence
+ july
+ racism
+ 2020
+ celebrate
+
+
+prediction
+[2 2 2]
--- clustering/knn.py
+++ clustering/knn.py
@@ -8,8 +8,8 @@ import glob
 import pandas as pd
 from numpy import random
 
-transcripts = glob.glob('../data/Trump/*.txt')
-#transcripts = glob.glob('../data/Biden/*.txt')
+#transcripts = glob.glob('../data/Trump/*.txt')
+transcripts = glob.glob('../data/Biden/*.txt')
 
 random.shuffle(transcripts)
 trainFrac = int(len(transcripts)*0.9)
@@ -22,56 +22,16 @@ for i in range(len(training)):
 	speech = open(training[i],'r')
 	words = []
 	for line in speech:
-		#print(line)
 		words += word_tokenize(line)
-		#tokens = word_tokenize(line)
-		#words.append(tokens)
 	seperator = ' '
 	speeches.append(seperator.join(words))
-
-#print(speeches[0])
-
-#print(speeches[0])
-#print('=================')
-#print(speeches[1])
-
-#tfidf_vectorizer=TfidfVectorizer(use_idf=True)
-#fitted_vectorizer = tfidf_vectorizer.fit_transform(speeches)
-#tfidf_vectorizer_vectors = fitted_vectorizer.transform(speeches)
 
 vectorizer = TfidfVectorizer()
 vectorized_speeches = vectorizer.fit(speeches)
 
 X = vectorizer.transform(speeches)
 
-#print(vectorizer.vocabulary_)
-#print(vectorizer.idf_)
-
-
-#X = tfidf_vectorizer_vectors
-
-
-#cv = CountVectorizer()
-
-#vectorizer = TfidfVectorizer(stop_words = 'english')
-#X = cv.fit_transform(speeches)
-#tfidf_transformer = TfidfTransformer(smooth_idf=True,use_idf=True)
-#tfidf_transformer.fit(X)
-#count_vector=cv.transform(speeches)
-#tf_idf_vec = tfidf_transformer.transform(count_vector)
-#X = vectorizer.fit_transform(speeches)
-
-#for k in range(4):
-
-#df_idf = pd.DataFrame(tfidf_transformer.idf_, index=cv.get_feature_names(),columns=["idf_weights"]
-
-#df_idf.sort_values(by=['idf_weights'])
-
-#feature_names = cv.get_feature_names()
-#first_doc_vec = tf_idf_vec[0]
-#print(first_doc_vec)
-
-k = 8	
+k = 5	
 model = KMeans(n_clusters=k, init='k-means++', max_iter=100, n_init=1)
 model.fit(X)
 
@@ -90,6 +50,13 @@ print("prediction")
 Y = vectorizer.transform(testing)
 prediction = model.predict(Y)
 print(prediction)
+
+
+for i in range(len(transcripts)):
+	Z = vectorizer.transform(transcripts)
+	print(transcripts[i][14:-4])
+	classification = model.predict(Z)
+	print(classification)
 
 #for i in range(len(testing)):
 #	Y = vectorizer.transform(testing[i])
